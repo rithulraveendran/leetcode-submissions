b1622fede84559d45641127b9d3ad52c96b4d915
Rithul <217548869+rithulraveendran@users.noreply.github.com>
2026-02-25 14:28:48 +0000

    Sync LeetCode submission Runtime - 11 ms (13.73%), Memory - 19.4 MB (25.03%)

---
+++ 0012-integer-to-roman/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        roman=''
+        signs=[[1000,'M'],[900,'CM'],[500,'D'],[400,'CD'],[100,'C'],[90,'XC'],[50,'L'],[40,'XL'],[10,'X'],[9,'IX'],[5,'V'],[4,'IV'],[1,'I']]
+        for n,s in signs:
+            if num//n:
+                roman+=s*(num//n)
+                num=num%n
+        return roman
